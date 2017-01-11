6d669da76abfafbfc142e46e8fd88d9eb92a7192
Eric Snell <eric_snell21@yahoo.com>
2017-01-11 14:28:44 -0500

    main app functionality and general styling

---
+++ assets/js/script.js
@@ -0,0 +1,30 @@
+$(document).ready(() => {
+
+  $(".search-button").click((event) => {
+    event.preventDefault();
+    let query = $(".search-field").val();
+    let userAgent = 'FreeCodeCamp Project https://codepen.io/ericsnell/pen/oYLBGW'
+
+    // AJAX request for Wikipedia articles containing user's query
+    $.ajax({
+      url: 'https://en.wikipedia.org/w/api.php?format=json&action=query&generator=search&gsrnamespace=0&gsrlimit=10&prop=extracts&pilimit=max&exintro&explaintext&exsentences=1&exlimit=max&gsrsearch=' + query,
+      type: 'GET',
+      data: query,
+      dataType: 'jsonp',
+      success: function(result) {
+        console.log('result -->', result);
+        $(".centered-searchbar").removeClass("centered-searchbar").addClass("top-searchbar");
+        $(".results").empty();
+        let results = result.query.pages;
+
+        for (var prop in results) {
+          $(".results").append("<li><a href='https://en.wikipedia.org/wiki?curid=" + results[prop].pageid + "' target='_blank'><div><h2>" + results[prop].title + "<h3><p>" + results[prop].extract + "</p></div></a></li>")
+        }
+      }
+    })
+    .fail((error) => {
+      console.log("Error: ", error);
+    })
+  });
+
+})
