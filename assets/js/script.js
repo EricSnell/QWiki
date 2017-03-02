cf0c725ce7ce164cac9a2681175f7e2ad679e48d
Eric Snell <eric_snell21@yahoo.com>
2017-03-02 17:37:44 -0500

    Converted styles to Sass using SMACSS file structure

--- assets/js/script.js
+++ assets/js/script.js
@@ -13,12 +13,12 @@ $(document).ready(() => {
       dataType: 'jsonp',
       success: function(result) {
         console.log('result -->', result);
-        $(".centered-searchbar").removeClass("centered-searchbar").addClass("top-searchbar");
+        $(".search-container").removeClass("center").addClass("top");
         $(".results").empty();
         let results = result.query.pages;
 
         for (var prop in results) {
-          $(".results").append("<li><a href='https://en.wikipedia.org/wiki?curid=" + results[prop].pageid + "' target='_blank'><div><h2>" + results[prop].title + "<h3><p>" + results[prop].extract + "</p></div></a></li>")
+          $(".results").append("<li><a href='https://en.wikipedia.org/wiki?curid=" + results[prop].pageid + "' target='_blank'><div class='wiki-article'><h2>" + results[prop].title + "<h3><p>" + results[prop].extract + "</p></div></a></li>")
         }
       }
     })
