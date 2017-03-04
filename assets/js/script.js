8e5016c1f0279abe7be8425860a6134911825661
Eric Snell <eric_snell21@yahoo.com>
2017-03-04 14:10:53 -0500

    Error message now appears if nothing is returned from users query

--- assets/js/script.js
+++ assets/js/script.js
@@ -2,6 +2,8 @@ $(document).ready(() => {
 
   $(".search-button").click((event) => {
     event.preventDefault()
+    $('p.error-text').remove()
+
     let query = $(".search-field").val()
     let userAgent = 'FreeCodeCamp Project https://codepen.io/ericsnell/pen/oYLBGW'
 
@@ -12,23 +14,28 @@ $(document).ready(() => {
       data: query,
       dataType: 'jsonp',
       success: function(result) {
-        console.log('result -->', result)
-        $(".search-container").animate({ marginTop: '50px' })
-        $(".results").empty();
-        let results = result.query.pages
+        if (result.query !== undefined) {
+          console.log('result -->', result)
+          $(".search-container").animate({ marginTop: '50px' })
+          $(".results").empty()
+          let results = result.query.pages
 
-        // Delays the display of articles
-        setTimeout(showResults, 400)
+          // Delays the display of articles
+          setTimeout(showResults, 400)
 
-        // Loops through results and adds each article as a list item, then fades them in in sequence
-        function showResults() {
-          for (var prop in results) {
-            $(".results").append($("<li><a href='https://en.wikipedia.org/wiki?curid=" + results[prop].pageid + "' target='_blank'><div class='wiki-article'><h2>" + results[prop].title + "<h3><p>" + results[prop].extract + "</p></div></a></li>").hide())
+          // Loops through results and adds each article as a list item, then fades them in in sequence
+          function showResults() {
+            for (var prop in results) {
+              $(".results").append($("<li><a href='https://en.wikipedia.org/wiki?curid=" + results[prop].pageid + "' target='_blank'><div class='wiki-article'><h2>" + results[prop].title + "<h3><p>" + results[prop].extract + "</p></div></a></li>").hide())
+            }
+
+            $(".results li").each(function(i) {
+              $(this).delay(100 * i).fadeIn()
+            })
           }
-
-          $(".results li").each(function(i) {
-            $(this).delay(100 * i).fadeIn()
-          })
+        } else {
+          $(".results").empty()
+          $(".search-container").append('<p class="error-text">Couldn\'t find anything. Try again?</p>')
         }
       }
     })
