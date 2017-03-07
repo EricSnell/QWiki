4402879748a3b777f39c49a7a254025c5fd8807d
Eric Snell <eric_snell21@yahoo.com>
2017-03-07 09:29:31 -0500

    ES6 syntax fixes

--- assets/js/script.js
+++ assets/js/script.js
@@ -2,46 +2,57 @@ $(document).ready(() => {
 
   $(".search-button").click((event) => {
     event.preventDefault()
-    $('p.error-text').remove()
 
+    /*-- Variables --*/
     let query = $(".search-field").val()
     let userAgent = 'FreeCodeCamp Project https://codepen.io/ericsnell/pen/oYLBGW'
 
-    // AJAX request for Wikipedia articles containing user's query
+    // Remove any displayed error message
+    $('p.error-text').remove()
+
+    // AJAX request to WIkiMedia API for Wikipedia articles containing user's query
     $.ajax({
-      url: 'https://en.wikipedia.org/w/api.php?format=json&action=query&generator=search&gsrnamespace=0&gsrlimit=10&prop=extracts&pilimit=max&exintro&explaintext&exsentences=1&exlimit=max&gsrsearch=' + query,
+      url: `https://en.wikipedia.org/w/api.php?format=json&action=query&generator=search&gsrnamespace=0&gsrlimit=10&prop=extracts&pilimit=max&exintro&explaintext&exsentences=1&exlimit=max&gsrsearch=${ query }`,
       type: 'GET',
       data: query,
       dataType: 'jsonp',
-      success: function(result) {
+      success(result) {
+
+        // If user's query returns results, display them. If not, show error message
         if (result.query !== undefined) {
           console.log('result -->', result)
-          $(".search-container").animate({ marginTop: '50px' })
-          $(".results").empty()
           let results = result.query.pages
 
-          // Delays the display of articles
+          $(".search-container").animate({ marginTop: '50px' })
+          $(".results").empty()
+
+          // Delays the display of articles after a short delay
           setTimeout(showResults, 400)
 
           // Loops through results and adds each article as a list item, then fades them in in sequence
           function showResults() {
             for (var prop in results) {
-              $(".results").append($("<li><a href='https://en.wikipedia.org/wiki?curid=" + results[prop].pageid + "' target='_blank'><div class='wiki-article'><h2>" + results[prop].title + "<h3><p>" + results[prop].extract + "</p></div></a></li>").hide())
+              $(".results").append(
+                $(`<li><a href='https://en.wikipedia.org/wiki?curid=${ results[prop].pageid }' target='_blank'><div class='wiki-article'><h2>${ results[prop].title }<h3><p>${ results[prop].extract }</p></div></a></li>`)
+                .hide()
+              )
             }
 
             $(".results li").each(function(i) {
               $(this).delay(100 * i).fadeIn()
             })
           }
+
         } else {
           $(".results").empty()
           $(".search-container").append('<p class="error-text">Couldn\'t find anything. Try again?</p>')
         }
       }
     })
-    .fail((error) => {
-      console.log("Error: ", error)
-    })
+
+    // Handles any AJAX error
+    .fail(error => console.log("Error: ", error))
+
   })
 
 })
